80fee6d472f8dfa190669b66458e3db1912c3368
osanow <osanow@wp.pl>
2019-02-03 18:45:51 +0100

    Removed redux store due to decide to delete login option

--- src/index.js
+++ src/index.js
@@ -1,37 +1,16 @@
 import React from 'react';
-// import ReactDOM from 'react-dom';
-import { render } from 'react-snapshot';
+import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware, compose, combineReducers } from 'redux';
-import createSagaMiddleware from 'redux-saga';
 
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-const composeEnhancers = ( process.env.NODE_ENV === 'development' ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ : null ) || compose;
-
-const rootReducer = combineReducers({
-    //reducers
-});
-
-const sagaMiddleware = createSagaMiddleware();
-
-const store = createStore(
-  //rootReducer,
-  composeEnhancers(applyMiddleware(sagaMiddleware))
-);
-
-//sagaMiddleware.run( watchContainer );
-
 const app = (
-    <Provider store={store}>
-        <BrowserRouter>
-            <App />
-        </BrowserRouter>
-    </Provider>
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>
 );
 
-render(app, document.getElementById('root'));
+ReactDOM.render(app, document.getElementById('root'));
 serviceWorker.register();
